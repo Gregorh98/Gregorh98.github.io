627d9d9674ab5e2a386b7644db033cdd7415451b
Gregor <gregor.hastings@outlook.com>
2024-10-03 00:39:26 +0100

    Tidied up

---
+++ halloween-2024/js/bats.js
@@ -0,0 +1,17 @@
+function create_bats(number) {
+    let snowflakes_container = document.getElementById("snowflakes");
+    for (let i = 1; i <= number; i++) {
+        let bat_id = "bat" + i;
+        snowflakes_container.innerHTML += `
+            <div class="snowflake">
+                <img src="assets/bat.gif" class="bat" id="${bat_id}" alt="Flying Bat" onclick="delete_bat('${bat_id}')">
+            </div>`;
+    }
+}
+
+function delete_bat(id)
+{
+    var audio = new Audio('assets/bat_splat.wav');
+    audio.play();
+    document.getElementById(id).hidden = true
+}
