1f337426163d00028d7b54226d35c74c9c12c718
Gregor <gregor.hastings@outlook.com>
2024-10-04 22:52:07 +0100

    Get weather data

--- can-I-ride/can-I-ride.js
+++ can-I-ride/can-I-ride.js
@@ -1,4 +1,13 @@
-function checkPostCode()
-{
-
+async function update() {
+    let url = "https://api.open-meteo.com/v1/forecast?latitude=55.8575&longitude=-3.169&hourly=temperature_2m,precipitation,wind_speed_10m,wind_gusts_10m&forecast_days=3"
+    try
+    {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data)
+    }
+    catch(error)
+    {
+        console.log(error)
+    }
 }
